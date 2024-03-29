61a6cf4b653b7d8b6272f2763dfa55c83fd9c1c9
Rohan-450 <zombiecpp106@gmail.com>
2024-03-30 01:15:58 +0530

    Quick Sort Added

---
+++ quickSort(1).cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+void print(int a[],int n){
+  for(int i=0;i<n;i++)
+    cout<<a[i]<<" ";
+  return;
+}
+void swap(int a[],int i,int j){
+  int temp=a[i];
+  a[i]=a[j];
+  a[j]=temp;
+  return;
+}
+int partition(int a[],int low,int high){
+  int pivot=a[high];
+  int i=low-1;
+  for(int j=low;j<high;j++){
+    if(a[j]<pivot){
+      i++;
+      swap(a,i,j);
+    }
+  }
+  swap(a,i+1,high);
+  return i+1;
+}
+void quickSort(int a[],int low,int high){
+  if(low<high){
+    int pi=partition(a,low,high);
+    quickSort(a,low,pi-1);
+    quickSort(a,pi+1,high);
+  }
+  return;
+}
+int main(){
+  int n;
+  cout<<"Enter the number of elements: ";
+  cin>>n;
+  int a[n];
+  cout<<"Enter the elements: ";
+  for(int i=0;i<n;i++)
+    cin>>a[i];
+  quickSort(a,0,n-1);
+  cout<<"Sorted array: ";
+  print(a,n);
+  return 0;
+}
+
---
+++ quickSort(2).cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int partition(int arr[],int low,int high){
+  int pivot=arr[low];
+  int i=low-1,j=high+1;
+  while(true){
+    do{
+      i++;
+    }while(arr[i]<pivot);
+    do{
+      j--;
+    }while(arr[j]>pivot);
+    if(i>=j)
+      return j;
+    swap(arr[i],arr[j]);
+  }
+}
+void quickSort(int arr[],int low,int high){
+  if(low<high){
+    int pi=partition(arr,low,high);
+    quickSort(arr,low,pi);
+    quickSort(arr,pi+1,high);
+  }
+}
+void print(int arr[],int n){
+  for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+  return;
+}
+int main(){
+  int n;
+  cout<<"Enter the number of elements: ";
+  cin>>n;
+  int arr[n];
+  cout<<"Enter the elements: ";
+  for(int i=0;i<n;i++)
+    cin>>arr[i];
+  quickSort(arr,0,n-1);
+  cout<<"Sorted array: ";
+  print(arr,n);
+  return 0;
+}
