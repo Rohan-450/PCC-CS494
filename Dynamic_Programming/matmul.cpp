08013f6215985969d84c7d98d51c1d5f39a34536
Rohan-450 <zombiecpp106@gmail.com>
2024-05-22 18:31:07 +0530

    Matrix Chain updated

--- Dynamic_Programming/matmul.cpp
+++ Dynamic_Programming/matmul.cpp
@@ -1,85 +1,85 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<iostream>
 using namespace std;
-int p[20];
-int M[20][20]={0};
-int S[20][20]={0};
-int n;
+#define INT_MAX 2147483647;
 
-void matrixChainOrder()
-{
-	
-	for(int i=1;i<=n;i++) 
-		M[i][i]=0;
+int p[100],cost[10][10],kvalue[10][10],n;
 
-	for(int len=2;len<n;len++)
-	{
-		for(int i=1;i<n-len+1;i++)
-		{
-			int j=i+len-1;
-			M[i][j]=999999;
-			for(int k=i;k<j;k++)
-			{
-				int cost=M[i][k]+M[k+1][j]+p[i-1]*p[k]*p[j];
-				if(cost<M[i][j]){
-					M[i][j]=cost;
-					S[i][j]=k;
-				}
-			}
-		}
-	}
+void initialize(){
+    int i,j;
+    for(i=1;i<n;i++){
+        for(j=1;j<n;j++){
+            if(i>=j){
+                cost[i][j]=0;
+                kvalue[i][j]=0;
+            }
+        }
+    }
+}
 
-    printf("\nM-table\n");
-	for(int i=1;i<=n;i++)
-	{
-		for(int j=1;j<=n;j++)
-		{
-			printf("%4d",M[i][j]);
-		}
+void matrixChain(){
+    int i,j,k,l;
+    initialize();
+    for(l=2;l<=n;l++){
+        for(i=1;i<=n-l+1;i++){
+            j=i+l-1;
+            cost[i][j]=INT_MAX;
+            for(k=i;k<=j;k++){
+                if(cost[i][j]>cost[i][k]+cost[k+1][j]+(p[i-1]*p[k]*p[j])){
+                    cost[i][j]=cost[i][k]+cost[k+1][j]+(p[i-1]*p[k]*p[j]);
+                    kvalue[i][j]=k;
+                }
+            }
+        }
+    }
+}
+
+void printCostMtx(){
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("%6d",cost[i][j]);
+        }
         printf("\n");
-	}
-
-    printf("\nS-table\n");
-	for(int i=1;i<=n;i++)
-	{
-		for(int j=1;j<=n;j++)
-		{
-			printf("%4d",S[i][j]);
-		}
-		printf("\n");
-	}
+    }
 }
 
+void printkvalues(){
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("%6d",kvalue[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-
-void printOptimalParenthesis(int i,int j)
-{
-	if(i==j) 
+void optimalParenthesise(int s[10][10],int i,int j){
+    if (i==j){
         printf("A%d",i);
-	else{
+    }
+    else{
         printf("(");
-		printOptimalParenthesis(i,S[i][j]);
-		printOptimalParenthesis(S[i][j]+1,j);
+        optimalParenthesise(s,i,s[i][j]);
+        optimalParenthesise(s,s[i][j]+1,j);
         printf(")");
-	}
+    }
 }
 
-
-int main()
-{
-	int i;
-	printf("Enter number of matrices\n");
+int main() {
+    int i;
+    printf("Enter no of matrices:");
     scanf("%d",&n);
- 	printf("Enter dimensions \n");
- 	for(i=0;i<n;i++)
-    {
-        printf("Enter d%d :: ",i);
+    printf("Enter matrix orders:");
+    for(i=0;i<=n;i++){
         scanf("%d",&p[i]);
     }
-	matrixChainOrder();
-    cout<<"Minimum number of multiplications are: ";
-    cout<< M[1][n - 1]<<endl;
-	printOptimalParenthesis(1,n-1);
-	printf("\n");
-	return 0;
+    matrixChain();
+	cout<<endl;
+    printCostMtx();
+	cout<<endl;
+	printkvalues();
+	cout<<endl;
+    optimalParenthesise(kvalue,1,n);
+    return 0;
 }
